d617ce2eb6b8d5a198e601abf2c7ad03cafd7af8
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-30 15:51:07 +0530

    use best practices

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,30 @@
+import { NextPage } from 'next'
+import { NextRouter, useRouter } from 'next/router'
+import { useEffect } from 'react'
+import useAuth from '../../hooks/useAuth'
+
+const Dashboard: NextPage = () => {
+  const router: NextRouter = useRouter()
+  const { user, Login, Logout } = useAuth({
+    ifNoUser: () => {
+      Login()
+    },
+  })
+
+  useEffect(() => {
+    if (user === null) return
+    console.log(user)
+    fetch(
+      `http:///localhost:3000/user_repo?username=${user.username}&token=${localStorage.accessToken}`
+    ).then(async (resp) => {
+      const json = await resp.json()
+
+      if (json.success === 0) {
+      }
+    })
+  }, [user])
+
+  return <div className="w-screen min-h-screen bg-black"></div>
+}
+
+export default Dashboard
